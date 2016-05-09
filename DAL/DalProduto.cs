69ba2e9d52e82e95234f336f2d9df6d51c69ed23
Tafarel Mello <tafarel.mello@gmail.com>
2016-05-09 19:31:11 -0300

    teste commit
    teste

---
+++ DAL/DalProduto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//add
+using System.Data;
+using System.Data.SqlClient;
+using Modelo;
+
+namespace DAL
+{
+    public class DalProduto
+    {
+        private DalConexao conexao;
+
+        public DalProduto(DalConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloProduto obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.objetoConexao;
+            cmd.CommandText = "insert into produto (pro_nome, pro_descricao, pro_valorpago, pro_valorvenda, pro_qtde, umed_cod, cat_cod, scat_cod)values (@nome, @descricao, @valorpago, @valorvenda, @qtde, @undmedcod, @catcod, @scatcod); select @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@nome", obj.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", obj.ProDescricao);
+            cmd.Parameters.AddWithValue("@valorpago", obj.ProValorPago);
+            cmd.Parameters.AddWithValue("@valorvenda", obj.ProValorVenda);
+            cmd.Parameters.AddWithValue("@qtde", obj.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", obj.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", obj.CatCod);
+            cmd.Parameters.AddWithValue("@scatcod", obj.ScatCod);
+            conexao.Conectar();
+            obj.ProCod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconecta();
+        }
+        public void Excluir(int codigo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.objetoConexao;
+            cmd.CommandText = "delete from produto where (pro_cod) = (@codigo)";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconecta();
+        }
+        public void Alterar(ModeloProduto obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.objetoConexao;
+            cmd.CommandText = "UPDATE Produto SET pro_nome = (@nome), pro_descricao = (@descricao), " +
+                "pro_valorpago = (@valorpago), pro_valorvenda = (@valorvenda), " +
+                "pro_qtde = (@qtde), umed_cod = (@undmedcod), cat_cod = (@catcod), " +
+                "scat_cod = (@scatcod) WHERE pro_cod = (@codigo) ";
+            cmd.Parameters.AddWithValue("@nome", obj.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", obj.ProDescricao);
+            cmd.Parameters.AddWithValue("@valorpago", obj.ProValorPago);
+            cmd.Parameters.AddWithValue("@valorvenda", obj.ProValorVenda);
+            cmd.Parameters.AddWithValue("@qtde", obj.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", obj.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", obj.CatCod);
+            cmd.Parameters.AddWithValue("@scatcod", obj.ScatCod);
+            cmd.Parameters.AddWithValue("@codigo", obj.ProCod);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconecta();
+        }
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT produto.cat_cod, produto.pro_nome, produto.pro_descricao, produto.pro_valorpago, produto.pro_valorvenda, produto.pro_qtde, undmedida.umed_nome, categoria.cat_nome, subcategoria.scat_nome from produto inner join undmedida on produto.umed_cod = undmedida.umed_cod inner join categoria on produto.cat_cod = categoria.cat_cod inner join subcategoria on produto.scat_cod = subcategoria.scat_cod where produto.pro_nome like '%" + valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloProduto CarregaModeloProduto(int codigo)
+        {
+            ModeloProduto modelo = new ModeloProduto();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.objetoConexao;
+            cmd.CommandText = "select * from Produto where (pro_cod) =" + codigo.ToString();
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.ProCod = Convert.ToInt32(registro["pro_cod"]);
+                modelo.ProNome = Convert.ToString(registro["pro_nome"]);
+                modelo.ProDescricao = Convert.ToString(registro["pro_descricao"]);
+                modelo.ProValorPago = Convert.ToDouble(registro["pro_valorpago"]);
+                modelo.ProValorVenda = Convert.ToDouble(registro["pro_valorvenda"]);
+                modelo.ProQtde = Convert.ToInt32(registro["pro_qtde"]);
+                modelo.UmedCod = Convert.ToInt32(registro["umed_cod"]);
+                modelo.CatCod = Convert.ToInt32(registro["cat_cod"]);
+                modelo.ScatCod = Convert.ToInt32(registro["scat_cod"]);
+            }
+            conexao.Desconecta();
+            return modelo;
+        }
+
+    }
+}
